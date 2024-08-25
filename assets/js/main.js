f35678ee5f819642fd74e86783aac7c164f24220
parvin <parvinnoori0@gmail.com>
2024-08-25 10:03:47 +0330

    handleFormSubmission and validate forms

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,54 @@ function showSwitchButton(buttonToShow, buttons) {
   buttonToShow.parentElement.classList.add("active");
 }
 
+// validate form
+function validateForm(form) {
+  const inputs = form.querySelectorAll("input:not([type='checkbox'])");
+  let isValid = true;
+
+  inputs.forEach((input) => {
+    const errorMessageElement = input.nextElementSibling;
+    if (!input.value.trim()) {
+      input.parentElement.classList.add("error"); // Add an error class for styling
+      isValid = false;
+
+      // show error message
+      if (
+        errorMessageElement &&
+        errorMessageElement.classList.contains("errorMessage")
+      ) {
+        errorMessageElement.textContent = `${input.name} is required.`;
+      }
+    } else {
+      input.parentElement.classList.remove("error");
+      // Hide error message
+      if (
+        errorMessageElement &&
+        errorMessageElement.classList.contains("errorMessage")
+      ) {
+        errorMessageElement.textContent = "";
+        errorMessageElement.style.display = "none";
+      }
+    }
+  });
+
+  return isValid;
+}
+// handle form submission
+function handleFormSubmission(e, form) {
+  e.preventDefault();
+
+  if (validateForm(form)) {
+    const formData = new FormData(form);
+    formData.forEach((key, value) => {
+      console.log(`${key}: ${value}`);
+    });
+    form.reset()
+  } else {
+    // console.log("validate failed");
+  }
+}
+
 // function to initialize form switching
 function initializeFormSwitching() {
   const loginForm = document.querySelector("#loginForm");
@@ -39,6 +87,13 @@ function initializeFormSwitching() {
     showForm(loginForm, forms);
     showSwitchButton(switchToSignupButton, buttons);
   });
+
+  loginForm.addEventListener("submit", (e) =>
+    handleFormSubmission(e, loginForm)
+  );
+  signupForm.addEventListener("submit", (e) =>
+    handleFormSubmission(e, signupForm)
+  );
 }
 
 document.addEventListener("DOMContentLoaded", initializeFormSwitching);
