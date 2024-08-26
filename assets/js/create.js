210101f1f8bd63166131ba0fe5fa33dc086237b8
parvin <parvinnoori0@gmail.com>
2024-08-26 14:55:54 +0330

    list.js

--- assets/js/create.js
+++ assets/js/create.js
@@ -1,29 +1,37 @@
 const taskForm = document.getElementById("create-task-form");
 const backBtn = document.getElementById("backStep");
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+export let tasks = JSON.parse(localStorage.getItem("tasks"))
+  ? JSON.parse(localStorage.getItem("tasks"))
+  : [];
 
-//tak form submission
-taskForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const formData = new FormData(taskForm);
-  const formDataObj = {};
+//task form submission
+if (taskForm) {
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(taskForm);
+    const formDataObj = {};
 
-  formData.forEach((value, key) => {
-    formDataObj[key] = value;
+    formData.forEach((value, key) => {
+      formDataObj[key] = value;
+      formDataObj.completed = false;
+    });
+
+    tasks.push(formDataObj);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    taskForm.reset();
+
+    window.location.href = "list.html";
+
+    // taskList();
   });
 
-  tasks.push(formDataObj);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  console.log("Tasks stored in local storage:", JSON.parse(localStorage.getItem("tasks")));
-  taskForm.reset();
+  //  handle back btn
+  function handleBackBtn(e) {
+    e.preventDefault();
+    taskForm.reset();
+    window.location.href = "index.html";
+  }
 
-  window.location.href = "list.html";
-});
-
-//  handle back btn
-function handleBackBtn(e) {
-  e.preventDefault();
-  taskForm.reset();
-  window.location.href = "index.html";
+  backBtn.addEventListener("click", (e) => handleBackBtn(e));
 }
-backBtn.addEventListener("click", (e) => handleBackBtn(e));
--- assets/js/list.js
+++ assets/js/list.js
@@ -1,5 +1,63 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-    console.log("Tasks retrieved from local storage on list page:", tasks);
-  });
-  
+import { tasks } from "./create.js";
+
+// dynamic task list
+function taskList() {
+  console.log(tasks);
+  const taskContainer = document.querySelector(".tab-pane.show.active");
+  tasks
+    .map((task) => {
+      const parentDiv = document.createElement("div");
+      parentDiv.className = "d-flex flex-column";
+      const labelWrapper = document.createElement("label");
+      labelWrapper.className =
+        "form-check-label w-100 d-flex align-items-center";
+      const Titlespan = document.createElement("span");
+      Titlespan.className = "task-title d-block";
+      Titlespan.textContent = task.taskName;
+      const dueDateSpan = document.createElement("span");
+      dueDateSpan.className = "text-muted";
+      dueDateSpan.textContent = "Due :" + task.dueDate;
+      const prioritySpan = document.createElement("span");
+      prioritySpan.className = "priarotyLabel ms-auto rounded-pill px-3 py-1";
+      switch (task.priaroty) {
+        case "low":
+          prioritySpan.classList.add("low");
+          break;
+        case "high":
+          prioritySpan.classList.add("high");
+          break;
+        case "medium":
+          prioritySpan.classList.add("md");
+          break;
+      }
+      prioritySpan.textContent = task.priaroty;
+      const formCheckInput = document.createElement("input");
+      formCheckInput.type = "checkbox";
+      formCheckInput.className = "form-check-input me-3";
+      formCheckInput.checked = task.completed;
+      if (task.completed) {
+        Titlespan.classList.add("text-decoration-line-through");
+      }
+      formCheckInput.addEventListener("change", () => {
+        task.completed = formCheckInput.checked;
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        const taskTitle =
+          formCheckInput.parentElement.querySelector(".task-title");
+        if (formCheckInput.checked) {
+          taskTitle.classList.add("text-decoration-line-through");
+        } else {
+          taskTitle.classList.remove("text-decoration-line-through");
+        }
+      });
+      parentDiv.appendChild(Titlespan);
+      parentDiv.appendChild(dueDateSpan);
+      labelWrapper.appendChild(formCheckInput);
+      labelWrapper.appendChild(parentDiv);
+      labelWrapper.appendChild(prioritySpan);
+      return labelWrapper;
+    })
+    .forEach((label) => {
+      taskContainer.appendChild(label);
+    });
+}
+document.addEventListener("DOMContentLoaded", taskList());
