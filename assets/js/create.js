f82ef6b2cfde49965f1a20402a6739fb5f197a27
parvin <parvinnoori0@gmail.com>
2024-08-27 20:35:57 +0330

    fix bug

--- assets/js/create.js
+++ assets/js/create.js
@@ -5,31 +5,32 @@ export let tasks = JSON.parse(localStorage.getItem("tasks"))
   : [];
 
 //task form submission
-taskForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+if (taskForm) {
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-  //form data
-  const formData = new FormData(taskForm);
-  const formDataObj = {};
+    //form data
+    const formData = new FormData(taskForm);
+    const formDataObj = {};
 
-  formData.forEach((value, key) => {
-    formDataObj[key] = value;
-    formDataObj.completed = false;
+    formData.forEach((value, key) => {
+      formDataObj[key] = value;
+      formDataObj.completed = false;
+    });
+
+    tasks.push(formDataObj);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    taskForm.reset();
+
+    window.location.href = "list.html";
   });
+  //  handle back btn
+  function handleBackBtn(e) {
+    e.preventDefault();
+    taskForm.reset();
+    window.location.href = "index.html";
+  }
 
-  tasks.push(formDataObj);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-
-  taskForm.reset();
-
-  window.location.href = "list.html";
-});
-
-//  handle back btn
-function handleBackBtn(e) {
-  e.preventDefault();
-  taskForm.reset();
-  window.location.href = "index.html";
+  backBtn.addEventListener("click", (e) => handleBackBtn(e));
 }
-
-backBtn.addEventListener("click", (e) => handleBackBtn(e));
